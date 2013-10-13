82f37f43a1f4b9f13e33c36d16f6b694dc2d28d8
A-Better-User <bdhair198@aol.com>
2013-10-13 10:55:37 -0400

    Added stripe functionality

--- application/controllers/FundsController.php
+++ application/controllers/FundsController.php
@@ -21,6 +21,26 @@ class FundsController extends Zend_Controller_Action
             $userModel = new Application_Model_User();
             $res = $userModel->addFundsToUser($user, $amount);
             
+            // Set your secret key: remember to change this to your live secret key in production
+            // See your keys here https://manage.stripe.com/account
+            Stripe::setApiKey("sk_test_mkGsLqEW6SLnZa487HYfJVLf");
+
+            // Get the credit card details submitted by the form
+            $token = $_POST['stripeToken'];
+
+            // Create the charge on Stripe's servers - this will charge the user's card
+            try {
+            $charge = Stripe_Charge::create(array(
+              "amount" => $amount, // amount in cents, again
+              "currency" => "usd",
+              "card" => $token,
+              "description" => "payinguser@example.com")
+            );
+            } catch(Stripe_CardError $e) {
+              // The card has been declined
+                $this->getResponse()->setHttpResponseCode(500);
+            }
+
             $response['status'] = '200';
             $response['free'] = '$' . number_format($user['money']['free'] + $amount, 2);
         }
@@ -31,6 +51,5 @@ class FundsController extends Zend_Controller_Action
         
         die(json_encode($response));
     }
-
 }
 
